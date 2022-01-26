1e8c15602ad49e97b23bc986cf2122a430b59bb6
Mamdasan S <mhsaber1375@gmail.com>
2022-01-26 07:07:11 +0100

    Add Postgres write to database option

--- crawler.py
+++ crawler.py
@@ -4,12 +4,11 @@ import threading
 from requests import Session
 from bs4 import BeautifulSoup
 
-from  lucernefestival_crawler_functions import get_event_list as get_event_list_lucernefestival
-from  lucernefestival_crawler_functions import parse_event_list as parse_event_list_lucernefestival
+from  lucernefestival import Lucernefestival_grabber, Lucernefestival_postgres
 
 
 class EventCrawler:
-    def __init__(self, url):
+    def __init__(self, url, save_to_database=False):
         self.url = url
         self.url_base =  urlparse(self.url).hostname
         self.url_scheme =  urlparse(self.url).scheme
@@ -18,13 +17,16 @@ class EventCrawler:
                                     {
                                     "get_event_list": self._get_event_list_of_lucernefestival,
                                     "parse_event_list": self._parse_event_list_of_lucernefestival,
+                                    "write_to_database": Lucernefestival_postgres.write_to_database,
                                     }
                                 }
+        self._save_to_database = save_to_database
         self._event_list = None
         self._get_event_list = None
         self._parse_event_list = None
         self.concurrent_threads = None
         self._data = []
+
 
     def _is_supported(self):
         if self.url_base in self._supported_methods:
@@ -32,28 +34,18 @@ class EventCrawler:
         else:
             return False
 
-    def crawl(self, **kwargs):
-        if not self._is_supported():
-            raise ValueError(f"This url is not supported!\nPlease include the internet protocol,e.g. http https, in url.\nSupported urls: {self._supported_methods}")
-        self._get_event_list   = self._supported_method_functions[f"{self.url_base}"]["get_event_list"]
-        self._parse_event_list = self._supported_method_functions[f"{self.url_base}"]["parse_event_list"]
 
-        self._get_event_list()
-        self._parse_event_list(**kwargs)
-        return self._data
-
-
-        
 
     def _get_event_list_of_lucernefestival(self):
-        self._event_list = get_event_list_lucernefestival(self.url)
+        self._event_list = Lucernefestival_grabber.get_event_list(self.url)
         return 
+
 
     def _parse_event_list_of_lucernefestival(self, expanded_details=False):
         def _parser(expanded_details, queue, thread_no):
             while True:
                 event = queue.get()
-                self._data.append(parse_event_list_lucernefestival(expanded_details, event, self.url))
+                self._data.append(Lucernefestival_grabber.parse_event_list(expanded_details, event, self.url))
                 queue.task_done()
         
         queue = Queue()
@@ -67,11 +59,29 @@ class EventCrawler:
 
         for event in self._event_list:
             queue.put(event)
+
         queue.join()
+        return
+
+    def crawl(self, **kwargs):
+        if not self._is_supported():
+            raise ValueError(f"This url is not supported!\nPlease include the internet protocol,e.g. http https, in url.\nSupported urls: {self._supported_methods}")
+        self._get_event_list   = self._supported_method_functions[f"{self.url_base}"]["get_event_list"]
+        self._parse_event_list = self._supported_method_functions[f"{self.url_base}"]["parse_event_list"]
+
+        self._get_event_list()
+        self._parse_event_list(**kwargs)
+
+        if self._save_to_database:
+            write_to_database = self._supported_method_functions[f"{self.url_base}"]["write_to_database"]
+            write_to_database(self._data)
+
+        return self._data
 
 
-ecrawler_summer = EventCrawler("https://www.lucernefestival.ch/en/program/summer-festival-22")
-print(ecrawler_summer.crawl(expanded_details=False))
+ecrawler_summer = EventCrawler("https://www.lucernefestival.ch/en/program/summer-festival-22", save_to_database=False)
+data = ecrawler_summer.crawl(expanded_details=True)
+
 
 # ecrawler_spring = EventCrawler("https://www.lucernefestival.ch/en/program/mendelssohn-festival-22")
 # print(ecrawler_spring.crawl(expanded_details=True))
